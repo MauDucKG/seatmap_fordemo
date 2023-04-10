c3ce5fdfa8ff98eedc420cb559f8e0bed5b00b33
Nguyễn Mậu Đức <nguyenmauduc91@gmail.com>
2023-04-10 22:56:33 +0700

    duc: change url for demo

--- src/Shared/InternalServices.js
+++ src/Shared/InternalServices.js
@@ -1,10 +1,10 @@
 import restAPI from "axios";
 import urlResource from './UrlResource'
 
-export const USER_API = "https://seatmap-database.herokuapp.com/user";
-export const MAP_API = "https://seatmap-database.herokuapp.com/map";
-export const LOGIN_API = "https://seatmap-database.herokuapp.com/user/login";
-export const LOGOUT_API = "https://seatmap-database.herokuapp.com/user/logout";
+export const USER_API = "https://mauduckg.onrender.com/user";
+export const MAP_API = "https://mauduckg.onrender.com/map";
+export const LOGIN_API = "https://mauduckg.onrender.com/user/login";
+export const LOGOUT_API = "https://mauduckg.onrender.com/user/logout";
 export const listRoles = {'Human Resources': 'hr', 'Developer': 'dv', 'Quality Assurance': 'qa'}
 export const headers = {
     Authorization: localStorage.getItem("token"),
